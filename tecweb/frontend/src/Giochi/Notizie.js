327e278dbdfde2b163d2c76fa86a68ccee4e3450
C3sco <c3sco1@gmail.com>
2022-11-27 14:09:25 +0100

    notizie

--- tecweb/frontend/src/Giochi/Notizie.js
+++ tecweb/frontend/src/Giochi/Notizie.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styleCard.css"
+import "./notiziaStyle.css"
 
 const AnimalNews = () => {
     let newsArray = connect();
