e7730a4b835e0b2310c07cc252feab5e6d12f3cc
C3sco <c3sco1@gmail.com>
2023-02-09 08:53:02 +0100

    laaaast

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
-//import reportWebVitals from './reportWebVitals';
-import axios from "axios";
 
-
-axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
