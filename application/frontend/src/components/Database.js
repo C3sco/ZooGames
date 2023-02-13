af8da7c2af0ff25fa472d4544a431f478fab42d4
Francesco <82574032+C3sco@users.noreply.github.com>
2023-02-13 17:54:47 +0100

    Update Database.js

--- application/frontend/src/components/Database.js
+++ application/frontend/src/components/Database.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://whkmnudljhmdydutsqzx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indoa21udWRsamhtZHlkdXRzcXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEyNzU3OTMsImV4cCI6MTk4Njg1MTc5M30.636dYt5e5vcK5IR69s_x0IR9jI_wdOP8hFO78MEuU3U'
+const supabaseUrl = 'YOUR URL'
+const supabaseAnonKey = 'YOUR KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
